579d182edd914785ff1a3239f8a6f5f4a76b803b
Vinay Naidu <vinay.naidu@gmail.com>
2019-02-14 15:19:46 +0000

    adding inbox data

--- src/app/components/inbox/inbox.component.ts
+++ src/app/components/inbox/inbox.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as data from './inbox-data';
+
 @Component({
   selector: 'app-inbox',
   templateUrl: './inbox.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InboxComponent implements OnInit {
 
+  inboxData: any[];
+  displayedColumns: string[];
+
   constructor() { }
 
   ngOnInit() {
+    this.getInboxData();
+  }
+
+  getInboxData() {
+    this.displayedColumns = ['caseNumber', 'owner', 'identifiedBy', 'caseStatus', 'taskId', 'criticality'];
+    this.inboxData =  data.inboxData;
   }
 
 }
