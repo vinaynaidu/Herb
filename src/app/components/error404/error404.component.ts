5e23f904825529f37c7b2a1f8c360eb8d2042adb
Vinay <vinaynaidu.uk@gmail.com>
2019-02-20 19:15:05 +0000

    added timestamp expiry logic to login session

--- src/app/components/error404/error404.component.ts
+++ src/app/components/error404/error404.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-error404',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Error404Component implements OnInit {
 
+  imagePath: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.imagePath = this.getSkeletorPath();
+  }
+
+  getSkeletorPath(): string {
+    let filenumber = _.random(1,9, false);
+    let extension = filenumber == 2 ? 'gif': 'jpg';
+    return `https://s3-eu-west-1.amazonaws.com/backup.vkn/hcl-db/herb/e404-${filenumber}.${extension}`;
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+
 import * as _ from 'lodash';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +16,12 @@ export class AuthService {
 
   private userCreds: any;
   private authKey: string;
+  private minutesToExpire: number;
 
   constructor() {
-    this.authKey = btoa('isUserAuthenticated');
+    this.authKey = btoa('isUserAuthenticated').replace(/=/g, '');
+    this.minutesToExpire = 120; // 2 hours
+
     this.userCreds = {
       //          kay     kaylynan        lynakaya
       username: ['a2F5', 'a2F5bHluYW4=', 'bHluYWtheWE='],
@@ -29,11 +34,21 @@ export class AuthService {
   public isUserAuthenticated(): boolean {
     let auth = localStorage.getItem(this.authKey);
 
-    if (!!auth) {
-      return _.get(JSON.parse(atob(auth)), 'isAuthenticated', false);
+    if (_.isEmpty(auth)) {
+      // Not logged in
+      return false;
     }
 
-    return false;
+    let decoded = JSON.parse(atob(auth));
+
+    if (!this.isValidTime(_.get(decoded, 'timestamp', null))) {
+      // Session is too old. expire it
+      localStorage.clear();
+      return false;
+    }
+
+    // Finally check the session is authenticated
+    return _.get(decoded, 'isAuthenticated', false);
   }
 
   // Sets local storage details if creds are valid
@@ -42,10 +57,16 @@ export class AuthService {
 
     if (isValid) {
       // Store details in local storage
-      localStorage.setItem(this.authKey, btoa(JSON.stringify({ username, password, isAuthenticated: true })));
+      localStorage.setItem(this.authKey, btoa(JSON.stringify({ username, password, isAuthenticated: true, timestamp: `${new Date().toISOString()}` })));
     }
 
     return isValid;
 
+  }
+
+  private isValidTime(timeframe: string): boolean {
+    let ma = moment(timeframe);
+    let mb = moment(new Date());
+    return mb.diff(ma, 'minutes') < this.minutesToExpire;
   }
 }
